db79c1c4ba40097f250a21100f8f9b5dd6ff2248
madebykraman <madebykraman@gmail.com>
2026-09-04 00:40:52 +0530

    design: apply private workspace visual overrides

--- app/u/[token]/page.tsx
+++ app/u/[token]/page.tsx
@@ -1,8 +1,9 @@
 import WorkspaceV5 from './workspace-v5'
 import ProjectStatus from './project-status'
 import ClientExperience from './client-experience'
+import ClientVisualOverrides from './client-visual-overrides'
 
 export default async function ClientProjectPage({ params }: { params: Promise<{ token: string }> }) {
   const { token } = await params
-  return <ClientExperience token={token}><ProjectStatus token={token} /><WorkspaceV5 token={token} /></ClientExperience>
+  return <><ClientVisualOverrides/><ClientExperience token={token}><ProjectStatus token={token} /><WorkspaceV5 token={token} /></ClientExperience></>
 }
