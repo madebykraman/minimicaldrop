b9ef2359630dfdf805c4ef10e0295dc7e5d95e4f
madebykraman <madebykraman@gmail.com>
2026-09-03 21:58:35 +0530

    feat: surface project delivery status in client portal

--- app/u/[token]/page.tsx
+++ app/u/[token]/page.tsx
@@ -1,6 +1,7 @@
 import WorkspaceV5 from './workspace-v5'
+import ProjectStatus from './project-status'
 
 export default async function ClientProjectPage({ params }: { params: Promise<{ token: string }> }) {
   const { token } = await params
-  return <WorkspaceV5 token={token} />
+  return <><ProjectStatus token={token} /><WorkspaceV5 token={token} /></>
 }
