f3b26ff76960855aa1d697f9a213e08d850da1ca
madebykraman <madebykraman@gmail.com>
2026-09-04 02:08:52 +0530

    mount v0.4 client delivery intelligence

--- app/u/[token]/page.tsx
+++ app/u/[token]/page.tsx
@@ -1,8 +1,8 @@
 import WorkspaceV5 from './workspace-v5'
-import ProjectStatus from './project-status'
+import DeliveryPanel from './delivery-panel'
 import ClientExperience from './client-experience'
 
 export default async function ClientProjectPage({ params }: { params: Promise<{ token: string }> }) {
   const { token } = await params
-  return <ClientExperience><ProjectStatus token={token} /><WorkspaceV5 token={token} /></ClientExperience>
+  return <ClientExperience><DeliveryPanel token={token} /><WorkspaceV5 token={token} /></ClientExperience>
 }
