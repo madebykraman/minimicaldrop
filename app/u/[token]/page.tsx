530b795b2cc06d924ee0ed04a718034892b64b19
madebykraman <madebykraman@gmail.com>
2026-09-04 01:18:40 +0530

    remove onboarding visual overrides from client workspace

--- app/u/[token]/page.tsx
+++ app/u/[token]/page.tsx
@@ -1,9 +1,8 @@
 import WorkspaceV5 from './workspace-v5'
 import ProjectStatus from './project-status'
 import ClientExperience from './client-experience'
-import ClientVisualOverrides from './client-visual-overrides'
 
 export default async function ClientProjectPage({ params }: { params: Promise<{ token: string }> }) {
   const { token } = await params
-  return <><ClientVisualOverrides/><ClientExperience token={token}><ProjectStatus token={token} /><WorkspaceV5 token={token} /></ClientExperience></>
+  return <ClientExperience><ProjectStatus token={token} /><WorkspaceV5 token={token} /></ClientExperience>
 }
