39da91bd7b7518f5dffc71f19f55d522846997c8
madebykraman <madebykraman@gmail.com>
2026-09-04 01:41:08 +0530

    load restored homepage styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { DM_Mono, Manrope, Space_Grotesk } from 'next/font/google'
 import GlobalChrome from '@/components/global-chrome'
 import './globals.css'
 import './brand.css'
+import './home.css'
 import './drop.css'
 import './admin/admin.css'
 
